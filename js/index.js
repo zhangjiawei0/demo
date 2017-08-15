cd85ff533199cb66f2ccea175e772a9b606e0790
zhangjiawei0 <15231120650@sina.cn>
2017-08-15 20:14:36 +0800

    +++

--- js/index.js
+++ js/index.js
@@ -7,31 +7,17 @@ $(function(){
 
     $('.audio').on('click',function() {
         if($('#media')!==null){
-            if($('#media').paused){
+            if($('#media').get(0).paused){
                 $('#media').get(0).play();
-                $(this).addClass('play');
             }else{
                $('#media').get(0).pause();
-               $(this).removeClass('play');
             }
+            $(this).toggleClass('play');
         }
     })
 
     var mySwiper = new Swiper('.swiper-container', {
         mode: 'vertical',
-        loop: true,
+        // loop: true,
     })
-
-    var c = $('#myCanvas');
-    c.width = $(document).width();
-    c.height = $(document).height();
-    c.attr('width',c.width);
-    c.attr('height',c.height);
-    var ctx = c.get(0).getContext("2d");
-    var img = new Image();  
-    img.src = "images/B12.jpg";
-    img.onload = function(e){
-        e.preventDefault();
-        ctx.drawImage(img, 0, 0, c.width, c.height);  
-    }
 })
